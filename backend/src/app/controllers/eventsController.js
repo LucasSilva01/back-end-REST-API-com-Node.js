7aa35b99268032afe1ee78c62e62f84896508212
LucasSilva01 <santoslucas9956@gmail.com>
2021-04-22 19:49:39 -0300

    some changes

--- backend/src/app/controllers/eventsController.js
+++ backend/src/app/controllers/eventsController.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router(); 
 const authMiddleware = require('../middlewares/auth')
+const Event = require('../models/events');
 
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ ok: true});
+router.get('/', async (req, res) => {
+    res.send({ 
+        ok: true,
+        user: req.params.userId
+    });
 
 });
+
+router.get('/:eventId', async (req, res) => {
+    res.send({ user: req.userId });
+})
+
+router.post('/', async (req, res) => {
+    res.send({ user: req.userId });
+})
+
+router.put('/:eventId', async (req, res) => {
+    res.send({ user: req.userId });
+})
+
+router.delete('/:eventId', async (req, res) => {
+    res.send({ user: req.userId });
+})
 
 module.exports = (app) => app.use('/events', router);
